bf059f8211fbb40b02d35c465f2bc4292f911a98
Gerard . <gerardmccann94@hotmail.co.uk>
2026-03-09 16:14:17 +0000

    Added PostsController Index action + view

---
+++ BlogPlatform/Controllers/PostsController.cs
@@ -0,0 +1,27 @@
+﻿using BlogPlatform.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogPlatform.Controllers
+{
+    public class PostsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PostsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var posts = await _context.Posts
+                .Include(p => p.Author)
+                .OrderByDescending(p => p.CreatedAt)
+                .ToListAsync();
+
+
+            return View(posts); ;
+        }
+    }
+}
---
+++ BlogPlatform/Views/Posts/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlogPlatform.Views.Posts
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
