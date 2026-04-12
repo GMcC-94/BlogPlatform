8ea0c9273ad1c3e921e4d958043146ed2fb271e2
Gerard . <gerardmccann94@hotmail.co.uk>
2026-04-12 12:58:50 +0100

    Add comment variables

---
+++ BlogPlatform/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BlogPlatform.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Body { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string AuthorId { get; set; }
+        public IdentityUser Author { get; set; }
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+    }
+}
