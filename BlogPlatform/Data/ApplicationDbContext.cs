85ddaf20c6d6098aa4f506689333b147b4db95c5
Gerard . <gerardmccann94@hotmail.co.uk>
2026-04-14 21:26:08 +0100

    Configured model relationships

--- BlogPlatform/Data/ApplicationDbContext.cs
+++ BlogPlatform/Data/ApplicationDbContext.cs
@@ -12,5 +12,17 @@ namespace BlogPlatform.Data
         }
 
         public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Comment>()
+                .HasOne(c => c.BlogPost)
+                .WithMany(p => p.Comments)
+                .HasForeignKey(c => c.PostId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
     }
 }
